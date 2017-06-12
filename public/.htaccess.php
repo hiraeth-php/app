259814a0744c547d9a14c261be25bf44f5775d62
Matthew J. Sahagian <msahagian@dotink.org>
2017-06-11 23:46:49 -0700

    Normalize static file handling

---
+++ public/.htaccess.php
@@ -0,0 +1,36 @@
+<?php
+
+//
+// CLI Server compatibility
+//
+
+$extension = pathinfo($_SERVER['REQUEST_URI'], PATHINFO_EXTENSION);
+$filepath  = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'];
+
+if ($extension == 'html') {
+	if (!is_dir($filepath)) {
+		header('HTTP/1.1 301 Moved Permanently');
+		header('Location: ' . substr($_SERVER['REQUEST_URI'], 0, -5));
+		exit(0);
+	}
+}
+
+if (!is_file($_SERVER['DOCUMENT_ROOT'] . '/.nostatic')) {
+	if (!is_dir($filepath)) {
+		if (is_file($filepath . '.html')) {
+			include($filepath . '.html');
+			exit(0);
+		}
+	} else {
+		if (is_file($filepath . '/index.html')) {
+			include($filepath . '/index.html');
+			exit(0);
+		}
+	}
+}
+
+if (is_file($filepath)) {
+	return FALSE;
+}
+
+include($_SERVER['DOCUMENT_ROOT'] . '/index.php');
